0e322842e5d8bd75986f0ab4e6c99e625aec0817
hafijur2005 <m.mdhafijur2005@gmail.com>
2026-02-23 21:55:45 +0600

    add heap sort

---
+++ Sort/Heap_sort.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld double
+#define fast ios::sync_with_stdio(false), cin.tie(nullptr)
+#define all(x) x.begin(), x.end()
+#define yes cout << "YES\n";
+#define no cout << "nO\n";
+#define nl cout << "\n";
+void heapify(int arr[], int n, int i)
+{
+    int max = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[max])
+        max = l;
+
+    if (r < n && arr[r] > arr[max])
+        max = r;
+
+    if (max != i)
+    {
+        swap(arr[i], arr[max]);
+        heapify(arr, n, max);
+    }
+}
+void heapSort(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i > 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main()
+{
+    fast;
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    heapSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
